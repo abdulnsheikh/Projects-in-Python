fdd3f9f54edcabf93fdd76abf35d725e4b12229e
Abdulnaser Sheikh <67299192+abdulnsheikh@users.noreply.github.com>
2021-01-11 21:43:54 -0600

    More Projects
    I uploaded more projects

---
+++ Automation/Copying '.jpg' and '.pdf' files into a newfolder/Selectivecopy.py
@@ -0,0 +1,18 @@
+# Abdulnaser Sheikh
+# https://www.linkedin.com/in/abdulnasersheikh/
+
+# Selective copy
+
+'''Walking through a folder tree and searching for files with a certain file extension('.jpg','.pdf')
+ and copying from whatever location they are present at to a new folder'''
+
+ import shutil
+
+ import os
+
+ # Walking through a folder tree
+ for folders,subfoldes,filenames in os.walk('/home/nitin'):
+ 	for filename in filenames:
+ 		if filename.endswith('.jpg') or filename.endswith('.pdf'):
+ 			shutil.copy(file_name,'/home/nitin/NEW_FOLDER')
+ 			
---
+++ Automation/Strong Password Detection/StrongPasswordDetection.py
@@ -0,0 +1,14 @@
+# Abdulnaser Sheikh
+# https://www.linkedin.com/in/abdulnasersheikh/
+
+# Strong Password Detection using regular expression
+
+import re
+
+def strong_pass(Password):
+
+	
+	if re.match(r'([a-zA-Z0-9]).{8,}', Password) is not None:
+		print ('Strong Password')
+	else:
+		print ('Weak Password')
